65ee1ecefc7465715af8532a5c53cd973e38500f
Rathwa Ronak <ronakrathwa69@gmail.com>
2025-07-04 22:55:36 +0530

    database chages

--- Database/DB.js
+++ Database/DB.js
@@ -3,7 +3,7 @@ const Mongoose=require ('mongoose');
 const connectDB = async () => {
   await Mongoose
     .connect(
-      "mongodb+srv://ronakrathwa69:d5JYcxldMlqmDvEn@cluster0.1xdvo.mongodb.net/AthleteX"
+      process.env.MONGODB_URI
     )
     .then(() => console.log("MongoDB connected:"))
     .catch((error) => console.log(error.message));
