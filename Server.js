b06edc0128b650b1fdee56a942bb94683a9aebe6
Rathwa Ronak <ronakrathwa69@gmail.com>
2025-07-18 19:36:16 +0530

    change in image upload

--- Server.js
+++ Server.js
@@ -31,7 +31,7 @@ app.use(
 //Server static files
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/static", express.static(path.join(__dirname, "static")));
-
+app.use('/Images', express.static(path.join(__dirname, 'public/Images')));
 
 //set view engines
 app.set("view engine", "ejs");
